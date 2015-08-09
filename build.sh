9c277178ed36df426cb2089f5ce2b0086842eba8
Daniel Bergey <bergey@teallabs.org>
2015-08-09 16:54:53 +0000

    add simple build script
    - cabal build
    - closure compiler with advanced optimizations
    - copy static files
    - run warp to preview result

---
+++ build.sh
@@ -0,0 +1,12 @@
+target=$1
+if [ "$target" == "" ]; then
+    echo "build.sh needs a build target"
+   exit 1
+fi
+dist=dist/build/$target/$target.jsexe
+cabal configure --ghc-options='-DGHCJS_BROWSER'
+cabal build $target
+echo "done compiling Haskell; running closure"
+ccjs $dist/all.js --compilation_level=ADVANCED_OPTIMIZATIONS  > $dist/all.min.js
+cp $target/static/* $dist
+warp -d $dist -p 8000
