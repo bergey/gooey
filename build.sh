8ad15a6b392f591d251f6244389a88c7e5ffc970
Daniel Bergey <bergey@teallabs.org>
2015-08-13 12:38:52 +0000

    timer: while developing, skip minification
    Closure is actually pretty slow

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ fi
 dist=dist/build/$target/$target.jsexe
 cabal configure --ghc-options='-DGHCJS_BROWSER'
 cabal build $target
-echo "done compiling Haskell; running closure"
-ccjs $dist/all.js --compilation_level=ADVANCED_OPTIMIZATIONS  > $dist/all.min.js
+# echo "done compiling Haskell; running closure"
+# ccjs $dist/all.js --compilation_level=ADVANCED_OPTIMIZATIONS  > $dist/all.min.js
 cp $target/static/* $dist
 warp -d $dist -p 8000
