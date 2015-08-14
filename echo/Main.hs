6e724377ad64407a8b67e5eba2da445b7a0eb654
Daniel Bergey <bergey@teallabs.org>
2015-08-14 17:03:00 +0000

    Add echo example

---
+++ echo/Main.hs
@@ -0,0 +1,35 @@
+-- |
+
+module Main where
+
+import           GHCJS.DOM
+import           GHCJS.DOM.Document
+import           GHCJS.DOM.Element
+import           GHCJS.DOM.EventTarget
+import           GHCJS.DOM.EventTargetClosures
+import           GHCJS.DOM.HTMLElement
+import           GHCJS.DOM.HTMLInputElement    (getValue)
+import           GHCJS.DOM.Types               hiding (Event)
+import           GHCJS.Foreign
+import           GHCJS.Foreign.Callback
+import           GHCJS.Marshal
+import           JavaScript.Web.AnimationFrame
+
+import           Control.Applicative
+import           Control.Concurrent
+import           Control.Monad                 hiding (sequence_)
+
+main :: IO ()
+main = do
+  Just doc <- currentDocument
+  Just body <- getBody doc
+  setInnerHTML body $ Just initialHtml
+  Just input <- fmap castToHTMLInputElement <$> getElementById doc "in"
+  Just out <- fmap castToHTMLElement <$> getElementById doc "out"
+  forever $ do
+    val <- getValue input :: IO (Maybe String)
+    setInnerText out val
+    threadDelay 500000
+
+initialHtml :: String
+initialHtml = "<input id=\"in\"></input><p id=\"out\"></p>"
