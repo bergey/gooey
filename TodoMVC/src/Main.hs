4d7700ad5adcd7db95ab52112256f40d9fee439f
Daniel Bergey <bergey@teallabs.org>
2015-08-17 22:36:23 +0000

    simplify readChan loop

--- TodoMVC/src/Main.hs
+++ TodoMVC/src/Main.hs
@@ -13,22 +13,15 @@ import           Control.Monad                 (forever, void)
 
 main :: IO ()
 main = do
-  actionQueue <- newChan
+  q <- newChan
   state <- newMVar ()
   -- loop, push to Chan
   void . forkIO . forever $ do
-    writeChan actionQueue ()
+    writeChan q ()
     threadDelay 1000000
   animate  -- requestAnimationFrame loop
-  -- fork event handler
-  void $ forkIO $ queueHandler state actionQueue
+  -- read, read from Chan
+  void . forkIO . forever $ readChan q
 
 animate :: IO ()
 animate = void $ inAnimationFrame ContinueAsync animate
-
--- | A helper function to hook an event handler to an event queue
-queueHandler :: MVar s -> Chan e -> IO ()
-queueHandler s q = do
-  ev <- readChan q
-  modifyMVar_ s return
-  queueHandler s q
