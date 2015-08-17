c0f66be377db02ea3b0981ce3d8aa254cf0b911e
Daniel Bergey <bergey@teallabs.org>
2015-08-17 22:47:38 +0000

    simplify more - shows bug

--- TodoMVC/src/Main.hs
+++ TodoMVC/src/Main.hs
@@ -1,20 +1,15 @@
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE QuasiQuotes       #-}
-
 module Main where
 
 import           Control.Concurrent
 import           Control.Concurrent.Chan
-import           Control.Concurrent.MVar
 import           Control.Monad           (forever, void)
 
 main :: IO ()
 main = do
   q <- newChan
-  state <- newMVar ()
   -- loop, push to Chan
   void . forkIO . forever $ do
     writeChan q ()
     threadDelay 1000000
   -- read, read from Chan
-  void . forkIO . forever $ readChan q
+  void . forever $ readChan q
