5c547ce879cbd88403c7ca777d740107292d94f0
Daniel Bergey <bergey@teallabs.org>
2015-08-17 22:37:43 +0000

    remove animation loop - shows bug

--- TodoMVC/src/Main.hs
+++ TodoMVC/src/Main.hs
@@ -3,13 +3,10 @@
 
 module Main where
 
-import           GHCJS.Foreign.Callback        (OnBlocked (..))
-import           JavaScript.Web.AnimationFrame (inAnimationFrame)
-
 import           Control.Concurrent
 import           Control.Concurrent.Chan
 import           Control.Concurrent.MVar
-import           Control.Monad                 (forever, void)
+import           Control.Monad           (forever, void)
 
 main :: IO ()
 main = do
@@ -19,9 +16,5 @@ main = do
   void . forkIO . forever $ do
     writeChan q ()
     threadDelay 1000000
-  animate  -- requestAnimationFrame loop
   -- read, read from Chan
   void . forkIO . forever $ readChan q
-
-animate :: IO ()
-animate = void $ inAnimationFrame ContinueAsync animate
