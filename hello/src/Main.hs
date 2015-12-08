fd506d842a730e425933fc0b31b97f0b2e55a2f0
Daniel Bergey <bergey@teallabs.org>
2015-12-07 23:35:55 -0500

    hello: update for ghcjs-0.2, ghcjs-dom-0.2

--- hello/src/Main.hs
+++ hello/src/Main.hs
@@ -4,10 +4,10 @@ module Main where
 
 import           GHCJS.DOM
 import           GHCJS.DOM.Document
-import           GHCJS.DOM.HTMLElement
+import           GHCJS.DOM.Element
 
 main :: IO ()
 main = do
   Just doc <- currentDocument
-  Just body <- documentGetBody doc
-  htmlElementSetInnerHTML body "<p/>Hello World</p>"
+  Just body <- getBody doc
+  setInnerHTML body $ Just "<p/>Hello World</p>"
