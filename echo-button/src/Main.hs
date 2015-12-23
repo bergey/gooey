2c2c4f7a5428d44ed1c10bb8ffbf711550ae0bd4
Daniel Bergey <bergey@teallabs.org>
2015-12-22 22:19:53 -0500

    echo-button: update for ghcjs-dom-0.2

--- echo-button/src/Main.hs
+++ echo-button/src/Main.hs
@@ -5,33 +5,27 @@ module Main where
 import           GHCJS.DOM
 import           GHCJS.DOM.Document
 import           GHCJS.DOM.Element
+import           GHCJS.DOM.EventTarget
 import           GHCJS.DOM.EventTargetClosures
-import           GHCJS.DOM.HTMLElement
-import           GHCJS.DOM.HTMLInputElement    (htmlInputElementGetValue)
+import           GHCJS.DOM.HTMLInputElement    (getValue)
 import           GHCJS.DOM.Types               hiding (Event)
-import           GHCJS.Foreign
-import           GHCJS.Marshal
-
-import           Control.Applicative
-import           Control.Concurrent
-import           Control.Monad                 hiding (sequence_)
 
 main :: IO ()
 main = do
   Just doc <- currentDocument
-  Just body <- documentGetBody doc
-  htmlElementSetInnerHTML body initialHtml
-  Just input <- fmap castToHTMLInputElement <$> documentGetElementById doc "in"
-  Just out <- fmap castToHTMLElement <$> documentGetElementById doc "out"
-  Just button <- documentGetElementById doc "button"
-  -- cb <- eventListenerNew (echo input out)
-  void $ eventTargetAddEventListener button "click" False $ echo input out
+  Just body <- getBody doc
+  setInnerHTML body $ Just initialHtml
+  Just inElem <- fmap castToHTMLInputElement <$> getElementById doc "in"
+  Just out <- getElementById doc "out"
+  Just button <- getElementById doc "button"
+  cb <- eventListenerNew $ echo inElem out
+  addEventListener button "click" (Just cb) False
 
 initialHtml :: String
 initialHtml = "<input id=\"in\"></input><p id=\"out\"></p><div id=\"button\" style=\"color: green; border:1px solid green;\">echo</>"
 
-echo :: HTMLInputElement -> HTMLElement -> Element -> MouseEvent -> IO ()
-echo input out _self _ev = do
-  val <- htmlInputElementGetValue input :: IO String
-  htmlElementSetInnerText out val
-  threadDelay 500000
+echo :: HTMLInputElement -> Element -> MouseEvent -> IO ()
+echo inElem out _ev = do
+  Just val <- getValue inElem :: IO (Maybe String)
+  setInnerHTML out $ Just val
+  -- threadDelay 500000
